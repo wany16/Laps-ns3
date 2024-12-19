c31c51188e98d2f111a4926f08eda5137ac17654
wany16 <1003378739@qq.com>
2024-12-19 18:06:04 +0800

    add the Test files

---
+++ simulation/runScript/Test/main.cc
@@ -0,0 +1,162 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#undef PGO_TRAINING
+#define PATH_TO_PGO_CONFIG "path_to_pgo_config"
+
+#include <iostream>
+#include <fstream>
+#include <unordered_map>
+#include <time.h>
+#include "ns3/core-module.h"
+#include "ns3/qbb-helper.h"
+#include "ns3/point-to-point-helper.h"
+#include "ns3/applications-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/global-route-manager.h"
+#include "ns3/ipv4-static-routing-helper.h"
+#include "ns3/packet.h"
+#include "ns3/error-model.h"
+#include <ns3/rdma.h>
+#include <ns3/rdma-client.h>
+#include <ns3/rdma-client-helper.h>
+#include <ns3/rdma-driver.h>
+#include <ns3/switch-node.h>
+#include <ns3/sim-setting.h>
+#include <ns3/userdefinedfunction.h>
+#include "ns3/trace-helper.h"
+
+using namespace ns3;
+using namespace std;
+
+NS_LOG_COMPONENT_DEFINE("CongestionControlSimulator");
+
+int main(int argc, char *argv[])
+{
+    // LogComponentEnable("userdefinedfunction", LOG_LEVEL_FUNCTION);
+    LogComponentEnable("CongestionControlSimulator", LOG_LEVEL_INFO);
+    LogComponentEnable("QbbNetDevice", LOG_LEVEL_FUNCTION);
+    // LogComponentEnable ("BEgressQueue", LOG_LEVEL_INFO);
+
+    // LogComponentEnable("SwitchNode", LOG_LEVEL_INFO);
+
+    
+
+
+
+    std::cout << "*******************************Parse the Default Parameters*****************************************" << std::endl;
+    global_variable_t varMap;
+    varMap.configFileName = "/file-in-ctr/inputFiles/A00083-test/CONFIG_DCQCN-test-00001.txt";  
+    parse_default_configures(&varMap);
+
+    std::cout << "*******************************Parse the Input Parameters*****************************************" << std::endl;
+    CommandLine cmd;
+
+    cmd.AddValue("topoFileName", "topoFileName", varMap.topoFileName);
+    cmd.AddValue("inputFileDir", "input File Directory", varMap.inputFileDir);
+    cmd.AddValue("outputFileDir", "output File Directory", varMap.outputFileDir);
+    cmd.AddValue("fileIdx", "fileIdx", varMap.fileIdx);
+    cmd.AddValue("simStartTimeInSec", "simStartTimeInSec", varMap.simStartTimeInSec);
+    cmd.AddValue("simEndTimeInSec", "simEndTimeInSec", varMap.simEndTimeInSec);
+    cmd.AddValue("qlenMonitorIntervalInNs", "qlenMonitorIntervalInNs", varMap.qlenMonitorIntervalInNs);
+    cmd.AddValue("lbsName", "Name of the load balancing solution.", varMap.lbsName);
+    cmd.AddValue("flowletTimoutInUs", "The time out of the flowlet in microsecond.", varMap.flowletTimoutInUs);
+    cmd.AddValue("loadRatio", "The load ratio of the server workload.", varMap.loadRatio);
+    cmd.AddValue("loadRatioShift", "The load AdjustFacror of the server workload.", varMap.loadRatioShift);
+    cmd.AddValue("workloadFile", "The load source of the workload.", varMap.workLoadFileName);
+    cmd.AddValue("patternFile", "The server run pattern.", varMap.patternFile);
+    cmd.AddValue("ccMode", "congestion control algorithm.", varMap.ccMode);
+    cmd.AddValue("screenDisplayInNs", "creen display interval in Ns.", varMap.screenDisplayInNs);
+    cmd.AddValue("enablePfcMonitor", "trace Pfc packets or not.", varMap.enablePfcMonitor);
+    cmd.AddValue("enableFctMonitor", "trace Fct or not", varMap.enableFctMonitor);
+    cmd.AddValue("enableQlenMonitor", "trace queue length or not.", varMap.enableQlenMonitor);
+    cmd.AddValue("rdmaAppStartPort", "minimal port for rdma client.", varMap.appStartPort);
+    cmd.AddValue("enableQbbTrace", "trace the packet event on node's all Qbb netdevices.", varMap.enableQbbTrace);
+    cmd.AddValue("testPktNum", "The number of packets to test.", varMap.testPktNum);
+
+    cmd.Parse(argc, argv);
+
+    std::cout << "*******************************Load the Default Parameters*****************************************" << std::endl;
+    load_default_configures(&varMap);
+
+    std::cout << "-------------------------------Create The Topology----------------------------------------" << std::endl;
+    create_topology_rdma(&varMap);
+    std::cout << "-------------------------------Assign The Addresses----------------------------------------" << std::endl;
+    assign_addresses(varMap.allNodes, varMap.addr2node);
+    std::cout << "-------------------------------Calculate The Paths----------------------------------------" << std::endl;
+    calculate_paths_for_servers(&varMap);
+    std::cout << "-------------------------------Install The Routing Table----------------------------------------" << std::endl;
+    install_routing_entries(&varMap);
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+    std::cout << "-------------------------------Configure The Switch----------------------------------------" << std::endl;
+    config_switch(&varMap);
+    std::cout << "-------------------------------Install The Application----------------------------------------" << std::endl;
+    install_rdma_client_on_node(&varMap, 3, 4, 1, varMap.testPktNum, varMap.appStartPort);
+    // install_rdma_client_on_node(&varMap, 5, 7, 1, varMap.testPktNum, varMap.appStartPort);
+
+    // install_rdma_client_on_node(&varMap, 6, 8, varMap.appStartPort+1);
+    // install_kv_cache_applications(&varMap);
+    /*
+    Ptr<Node> srcnode = varMap.svNodes.Get(0);
+    Ptr<Node> dstnode = varMap.svNodes.Get(1);
+    // 获取节点的第一个接口的IPv4地址
+    Ptr<Ipv4> srcipv4_1 = srcnode->GetObject<Ipv4>();
+    // uint32_t srcinterfaceIndex_1 = interfaces.GetInterfaceIndex(srcnode, 0);
+    Ipv4Address srcipAddr1 = srcipv4_1->GetAddress(1, 0).GetLocal();
+    // 获取节点2的第一个接口的IPv4地址
+    Ptr<Ipv4> dstipv4_1 = dstnode->GetObject<Ipv4>();
+    // uint32_t dstinterfaceIndex_1 = interfaces.GetInterfaceIndex(dstnode, 0);
+    Ipv4Address dstipAddr1 = dstipv4_1->GetAddress(1, 0).GetLocal();
+
+    std::cout << "-------------------------------Monitor The queue Len----------------------------------------" << std::endl;
+    monitor_special_port_qlen(&varMap, 0, 1, 0);
+    std::cout << "-------------------------------Monitor The qBB Device----------------------------------------" << std::endl;
+    set_QBB_trace(&varMap);
+    /*std::cout << "-------------------------------Monitor Device for pcap trace----------------------------------------" << std::endl;
+    std::string prefix = "rdma_pcap_trace";
+    NetDeviceContainer nodeDevices = get_all_netdevices_of_a_node(srcnode);
+
+    for (uint32_t i = 0; i < nodeDevices.GetN(); ++i)
+    {
+        std::stringstream ss;
+        ss << varMap.outputFileDir << "/" << prefix << "_" << i;
+        pcapHelper.EnablePcap(ss.str(), nodeDevices.Get(i));
+    }*/
+    std::cout << "-------------------------------Start the Simulation----------------------------------------" << std::endl;
+    Simulator::Stop(Seconds(varMap.simEndTimeInSec));
+    NS_LOG_INFO("Run Simulation.");
+    Simulator::Run();
+    // switchportinfoPrint(&varMap, 3);
+    // monitor rps lb node
+    // std::cout << map_to_string<uint32_t, uint32_t>(SwitchNode::m_rpsPortInf[3]) << std::endl;
+
+    /* //monitor ecmp node
+    for (uint32_t i = 1; i < varMap.swNodes.Get(3)->GetNDevices(); i++)
+    {
+        if (SwitchNode::m_ecmpPortInf[3].find(i) != SwitchNode::m_ecmpPortInf[3].end())
+        {
+        std::cout << "SwitchNode::m_ecmpPortInf[3] size: " << SwitchNode::m_ecmpPortInf[3].size() << ",port id is " << i << " " << map_to_string<uint32_t, uint32_t>(SwitchNode::m_ecmpPortInf[3][i]) << std::endl;
+
+        }
+
+    }
+    */
+    //  std::cout << map_to_string<uint32_t, uint32_t>(SwitchNode::m_rpsPortInf[3]) << std::endl;
+    sim_finish(&varMap);
+    Simulator::Destroy();
+    std::cout << "-------------------------------Finish The Simulation----------------------------------------" << std::endl;
+    return 0;
+}
---
+++ simulation/runScript/Test/run.py
@@ -0,0 +1,164 @@
+import os
+import sys
+import argparse
+import shutil
+import os
+import sys
+import argparse
+import shutil
+import time
+
+def override_files(src_dir, dst_dir):
+    # 确保目标目录存在
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir)
+    # 使用os.walk遍历源目录及其子目录
+    # root：当前目录的路径（字符串形式）。dirs：当前目录下子目录的名称列表（字符串列表）。files：当前目录下文件的名称列表（字符串列表）。
+    for root, dirs, files in os.walk(src_dir):
+        # 计算相对于源目录的相对路径
+        rel_root = os.path.relpath(root, src_dir)
+        dst_subdir = os.path.join(dst_dir, rel_root)
+        # 确保目标目录的子目录存在
+        if not os.path.exists(dst_subdir):
+            os.makedirs(dst_subdir)
+        # 遍历文件
+        for file in files:
+            src_file = os.path.join(root, file)
+            dst_file = os.path.join(dst_subdir, file)
+            # 如果目标文件不存在或源文件更新时间晚于目标文件，则复制源文件到目标目录
+            if not os.path.exists(dst_file) or os.path.getmtime(src_file) > os.path.getmtime(dst_file):
+                shutil.copy2(src_file, dst_file)
+                print(f"File {src_file} is copied to {dst_file}.")
+
+def get_dir_root_host():
+    if os.path.exists("/file-in-ctr/"):
+        return "/file-in-ctr/"
+    elif os.path.exists("/file-in-cntr/"):
+        return "/file-in-cntr/"
+    else:
+        print("No such directory: /file-in-ctr/ or /file-in-cntr/")
+        return None
+
+def update_module_files(dir_root_host, dir_root_ns3, sydDirList):
+    for sydDir in sydDirList:
+        override_files(dir_root_host + sydDir, dir_root_ns3 + sydDir)
+
+def get_experiment_details():
+    current_file_path = os.path.abspath(__file__)
+    current_dir_path = os.path.dirname(current_file_path)
+    current_dir_name = os.path.basename(current_dir_path)
+    experimentalName = current_dir_name
+    mainFileName = "main"
+    return experimentalName, mainFileName
+
+def update_main_file(dir_ns3_scratch, dir_host_runScript, mainFileName):
+    if mainFileName is None:
+        print("Failed to get experiment details.")
+        sys.exit(1)
+    target_mainFile_path = os.path.join(dir_ns3_scratch, mainFileName + ".cc")
+    source_mainFile_path = os.path.join(dir_host_runScript, mainFileName + ".cc")
+    if os.path.exists(target_mainFile_path):
+        os.remove(target_mainFile_path)
+    shutil.copy2(source_mainFile_path, target_mainFile_path)
+
+def create_directory(dstDir):
+    os.makedirs(dstDir, exist_ok=True)
+
+experimentalName, mainFileName = get_experiment_details()
+dir_host_root = get_dir_root_host()
+dir_host_workloads = dir_host_root + "simulation/" + "workloads/"
+create_directory(dir_host_workloads)
+dir_host_topologies = dir_host_root + "simulation/" + "topologies/"
+create_directory(dir_host_topologies)
+dir_host_results = dir_host_root + "simulation/" + "results/" + experimentalName + "/"
+create_directory(dir_host_results)
+dir_host_patterns = dir_host_root + "simulation/" + "patterns/"
+create_directory(dir_host_patterns)
+dir_host_configures = dir_host_root + "simulation/" + "configures/"
+create_directory(dir_host_configures)
+dir_host_runScript = dir_host_root + "simulation/" + "runScript/"
+create_directory(dir_host_runScript)
+dir_ns3_root = "/app/ns3-detnet-rdma-main/ns-3.33/"
+dir_ns3_scratch = dir_ns3_root + "scratch/"
+
+
+# 调用函数更新模块文件
+sydDirList = ['src']
+update_module_files(dir_host_root, dir_ns3_root, sydDirList)
+# 调用函数获取实验详情
+# update the main.cc
+update_main_file(dir_ns3_scratch, dir_host_runScript, mainFileName)
+
+
+
+#
+
+parser = argparse.ArgumentParser(description="请输入以下参数")
+parser.add_argument("--configFileName", default="CONFIG_DCQCN-test-00001.txt", help="defaultFileName, by default CONFIG.txt")
+parser.add_argument("--topoFileName", default="TOPO-test-00001.txt",  help="defaultFileName, by default fat_tree_topology.txt")
+parser.add_argument("--simStartTimeInSec", default="0", help="simulation start time")
+parser.add_argument("--simEndTimeInSec", default="0.005",  help="simulation end time")
+parser.add_argument("--flowLunchEndTimeInSec", default="0.001", help="flow end time")
+parser.add_argument("--qlenMonitorIntervalInNs", default="100000", help="Qlen Monitor period In Ns")
+parser.add_argument("--lbsName", default="RPS", help="Load balancing algorithm")
+parser.add_argument("--flowletTimoutInUs", default="50", help="The time out of the flowlet in microsecond.")
+parser.add_argument("--loadRatioShift", default="1.0",  help="loadfactorAdjustFacror:Ring ->1,all2all->1/(n-1),Reduce->1/(K-1),n is host num,k is group num.")
+parser.add_argument("--PS", default="30",  help="Physical Server 30 is DCTCP_CDF',29 is RPC_CDF,28 is VL2_CDF")
+parser.add_argument("--ccMode", default="Dcqcn_mlx",  help="congestion control algorithm")
+parser.add_argument("--screenDisplayInNs", default="10000000",  help="screen display interval in Ns")
+parser.add_argument("--enablePfcMonitor", default="true",  help="trace Pfc packets or not ")
+parser.add_argument("--enableFctMonitor", default="true",  help="trace Fct or not")
+parser.add_argument("--enableQlenMonitor", default="false",  help="trace queue length or not")
+parser.add_argument("--rdmaAppStartPort", default="6666",  help="minimal port for rdma client")
+parser.add_argument("--enableQbbTrace", default="true",  help="trace the packet event on node's all Qbb netdevices")
+parser.add_argument("--testPktNum", default="1",  help="The number of packets to test")
+args = parser.parse_args()
+
+
+os.chdir(dir_ns3_root)
+
+Line_command = '\
+./waf --run "scratch/{}\
+--fileIdx={}\
+--outputFileDir={}\
+--topoFileName={}\
+--simStartTimeInSec={}\
+--qlenMonitorIntervalInNs={}\
+--simEndTimeInSec={}\
+--lbsName={}\
+--flowletTimoutInUs={}\
+--loadRatioShift={}\
+--ccMode={}\
+--screenDisplayInNs={}\
+--enablePfcMonitor={}\
+--enableFctMonitor={}\
+--enableQlenMonitor={}\
+--enableQbbTrace={}\
+--rdmaAppStartPort={}\
+--testPktNum={}\
+--loadRatio={} --workloadFile={} --patternFile={}"\
+'.format(
+    mainFileName,
+    experimentalName,
+    dir_host_results,
+    dir_host_topologies + args.topoFileName,
+    args.simStartTimeInSec,
+    args.qlenMonitorIntervalInNs,
+    args.simEndTimeInSec,
+    args.lbsName,
+    args.flowletTimoutInUs,
+    args.loadRatioShift,
+    args.ccMode,
+    args.screenDisplayInNs,
+    args.enablePfcMonitor,
+    args.enableFctMonitor,
+    args.enableQlenMonitor,
+    args.enableQbbTrace,
+    args.rdmaAppStartPort,
+    args.testPktNum,
+    args.loadratio,
+    dir_host_workloads + args.workloadFile,
+    dir_host_patterns + args.patternFile
+)
+print(Line_command)
+os.system(Line_command)
